e18ee2f5349720b1385db4416949d676a893772e
Jaqueline <jaqueline@schwencke.dk>
2024-03-19 20:44:30 +0100

    controller build

--- src/main/java/app/controllers/c3/ControllerC3.java
+++ src/main/java/app/controllers/c3/ControllerC3.java
@@ -1,8 +1,14 @@
 package app.controllers.c3;
 
+import app.entities.c3.Instructions;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.c3.InstructionsMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ControllerC3 {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool)
@@ -12,6 +18,16 @@ public class ControllerC3 {
 
     private static void index(Context ctx, ConnectionPool connectionPool)
     {
-        ctx.render("/c3/index.html");
+        List<Instructions> instructions = new ArrayList<>();
+
+        try {
+            instructions = InstructionsMapper.getAllInstructions(connectionPool);
+            ctx.attribute("instructionsList",instructions);
+            ctx.render("/c3/index.html");
+
+        } catch (DatabaseException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }

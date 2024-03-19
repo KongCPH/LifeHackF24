7f963747c07df37a46603f5c0953c17d816335b9
Jaqueline <jaqueline@schwencke.dk>
2024-03-19 21:15:12 +0100

    comments added

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -31,6 +31,6 @@ public class Main
         app.get("/", ctx -> ctx.render("index.html"));
         UserController.addRoutes(app, connectionPool);
         TimeZonesController.addRoutes(app, connectionPool);
-        ControllerC3.addRoutes(app, connectionPool);
+        ControllerC3.addRoutes(app, connectionPool); // controlleren indeholder routes til vores views, altså hvor viden er, forbindelse til det skabes her.
     }
 }

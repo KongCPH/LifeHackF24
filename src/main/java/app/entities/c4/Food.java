23e7e77792b793bf03faac19c10f7af84be8d449
m4cfly <nicklb@live.dk>
2024-03-19 23:43:50 +0100

    tilføjet entities, tilføjet Food klasse der indeholder attributer constructors og getters samt toSTring

---
+++ src/main/java/app/entities/c4/Food.java
@@ -0,0 +1,50 @@
+package app.entities.c4;
+
+public class Food
+{
+    private int foodId;
+    private String foodDescription;
+    private String foodMonth;
+    private String foodCategory;
+    private String foodItem;
+
+
+    public Food(int foodId, String foodDescription, String foodMonth, String foodCategory, String foodItem) {
+        this.foodId = foodId;
+        this.foodDescription = foodDescription;
+        this.foodMonth = foodMonth;
+        this.foodCategory = foodCategory;
+        this.foodItem = foodItem;
+    }
+
+    public int getFoodId() {
+        return foodId;
+    }
+
+    public String getFoodDescription() {
+        return foodDescription;
+    }
+
+    public String getFoodMonth() {
+        return foodMonth;
+    }
+
+    public String getFoodCategory() {
+        return foodCategory;
+    }
+
+    public String getFoodItem() {
+        return foodItem;
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" +
+                "foodId=" + foodId +
+                ", foodDescription='" + foodDescription + '\'' +
+                ", foodMonth='" + foodMonth + '\'' +
+                ", foodCategory='" + foodCategory + '\'' +
+                ", foodItem='" + foodItem + '\'' +
+                '}';
+    }
+}

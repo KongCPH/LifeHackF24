f324e49479b3c7d1107f9d4df285d346bd2fff08
Jenjolen <cph-jn466@cphbusiness.dk>
2024-03-20 13:38:15 +0100

    started c4UserController etc.

--- src/main/java/app/controllers/c4/c4UserController.java
+++ src/main/java/app/controllers/c4/c4UserController.java
@@ -2,6 +2,7 @@ package app.controllers.c4;
 
 import app.entities.Task;
 import app.entities.User;
+import app.entities.c4.Food;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.c4.FoodMapper;
@@ -10,24 +11,22 @@ import io.javalin.http.Context;
 
 import java.util.List;
 
-public class c4UserController
-{
-    public static void addRoutes(Javalin app, ConnectionPool connectionPool)
-    {
+public class c4UserController {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/c4", ctx -> index(ctx, connectionPool));
-        app.get("/search", ctx -> search(ctx, connectionPool));
+        app.post("/search", ctx -> search(ctx, connectionPool));
     }
 
     private static void search(Context ctx, ConnectionPool connectionPool) {
-        String taskName = ctx.formParam("taskname");
-        if (taskName.length() > 3) {
-            User user = ctx.sessionAttribute("currentUser"); // den kigger i vores session state og ser om der findes en key der hedder currentUser,
-            // hvilket der er hvis man er logget ind
+        String foodMonth = ctx.formParam("month");
+        String foodCategory = ctx.formParam("food_category");
+        if (!(foodMonth=="Choose month") || !(foodCategory=="Choose category")) {
+
             try {
-                Task newTask = FoodMapper.choose(user, taskName, connectionPool);
-                List<Task> taskList = TaskMapper.getAllTasksPerUser(user.getUserId(), connectionPool);
+                Food newFood = FoodMapper.choose(foodMonth, foodCategory, connectionPool);
+                List<Food> foodList = FoodMapper.choose();
                 ctx.attribute("taskList", taskList); // attribute bliver nulstillet ligeså snart vi er blevet sendt videre til task.html
-                ctx.render("task.html");
+                ctx.render("index.html");
             } catch (DatabaseException e) {
                 ctx.attribute("message", "Noget gik galt. Prøv evt. igen.");
                 ctx.render("task.html");
@@ -35,11 +34,9 @@ public class c4UserController
         }
 
 
-
     }
 
-    private static void index(Context ctx, ConnectionPool connectionPool)
-    {
+    private static void index(Context ctx, ConnectionPool connectionPool) {
         ctx.render("/c4/index.html");
     }
 }

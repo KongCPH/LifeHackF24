7f3ec6665291472584e161af5028656a54424a68
TobiasIpsen <hans7tobi@gmail.com>
2024-03-20 12:42:19 +0100

    Created getAllTasksPerTeam and MoveTassk on Task mapper. Created moveTask on Task Controller

--- src/main/java/app/controllers/c1controllers/C1TaskController.java
+++ src/main/java/app/controllers/c1controllers/C1TaskController.java
@@ -1,4 +1,38 @@
 package app.controllers.c1controllers;
 
+import app.entities.c1entities.C1Task;
+import app.entities.c1entities.C1Team;
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.persistence.c1persistence.C1TaskMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.List;
+
+import static app.persistence.c1persistence.C1TaskMapper.moveTask;
+
+
 public class C1TaskController {
+
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.post("movetask", ctx -> moveTask(ctx, connectionPool));
+    }
+
+    private static void moveTask(Context ctx, ConnectionPool connectionPool) {
+        C1Team team = ctx.sessionAttribute("team");
+
+        try{
+            int task_id = Integer.parseInt(ctx.formParam("task_id"));
+            int lifecycle = Integer.parseInt(ctx.formParam("lifecycle_id"));
+            C1TaskMapper.moveTask(task_id, lifecycle, connectionPool);
+            List<C1Task> taskList = C1TaskMapper.getAllTasksPerTeam(team.getTeamID(), connectionPool);
+            ctx.attribute("taskList", taskList);
+            ctx.render("dashboard.html");
+        } catch (DatabaseException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
--- src/main/java/app/persistence/c1persistence/C1TaskMapper.java
+++ src/main/java/app/persistence/c1persistence/C1TaskMapper.java
@@ -1,4 +1,64 @@
 package app.persistence.c1persistence;
 
+import app.entities.c1entities.C1Task;
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class C1TaskMapper {
-}
+
+    public static List<C1Task> getAllTasksPerTeam(int team_id, ConnectionPool connectionPool) throws DatabaseException {
+
+        List<C1Task> taskList = new ArrayList<>();
+        String sql = "SELECT * FROM c1_task WHERE team_id = ? ORDER BY task_id";
+
+        try(
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql);
+                ) {
+            ps.setInt(1, team_id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                int id = rs.getInt("task_id");
+                String title = rs.getString("title");
+                String description = rs.getString("description");
+                String user = rs.getString("user");
+                int usId = rs.getInt("us_id");
+                int lifecycleId = rs.getInt("lifecycle_id");
+                taskList.add(new C1Task(id, title, description, user, lifecycleId, team_id));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return taskList;
+    }
+
+    public static void moveTask(int task_id, int lifecycle_id, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "UPDATE c1_task SET lifecycle_id = ? WHERE task_id = ?";
+
+        try(
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql);
+        ) {
+            ps.setInt(1, lifecycle_id);
+            ps.setInt(2, task_id);
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1)
+            {
+                throw new DatabaseException("Fejl i opdatering af en task");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    } // METHOD END
+
+
+
+
+} // CLASS END
